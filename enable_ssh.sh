2b0184ef73f67d33e7d3caf3ad919ebbe34bfe12
DanteArcanaelMontford <dantearcanaelmontford@gmail.com>
2023-03-15 09:21:51 -0300

    feat: test3

--- enable_ssh.sh
+++ enable_ssh.sh
@@ -71,7 +71,10 @@ then
   echo "[+] Path to ssh config file $ssh_config_path"
   sleep 1
   echo "[+] Adding ssh port service on config file..."
+  # echo "[!] SSH Configuration Created by Morphus script"
   # echo "Port 22" >> $ssh_config_path
+  # echo "#PasswordAuthentication yes" >> $ssh_config_path
+  # PermitRootLogin yes
   ssh_file_port_line=$(grep "Port 22" -rnw /etc/ssh/sshd_config | cut -d ":" -f1)
   sleep 1
   # echo "[+] Port 22 config file added on line $ssh_file_port_line"
